1ecc0c508e65014eac64c4ba736e7f13eddcfb28
Timothy Vernon <timothy.n.vernon@gmail.com>
2020-03-01 21:40:30 +0100

    Correct css style support

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   verbose: true,
-  setupFilesAfterEnv: ['./setupTests.js']
+  setupFilesAfterEnv: ['./setupTests.js'],
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '^.+\\.css$': 'jest-transform-css',
+    '\\.(jpg|jpeg|png|gif|webp|svg)$': 'jest-transform-file'
+  }
 };
--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import './styles.css';
 
 function App() {
-  return <div>This is a simple React application</div>;
+  return (
+    <div>
+      <h1 style={{ marginTop: 0 }}>This is a simple React application</h1>
+    </div>
+  );
 }
 
 export default App;
